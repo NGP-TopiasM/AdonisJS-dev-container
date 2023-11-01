a303952030cf8305880feffe2b3c9eb862d648d2
NGP-TopiasM <97453539+NGP-TopiasM@users.noreply.github.com>
2023-11-01 14:17:52 +0100

    Update afterInitPostCreateCommands.sh

--- .devcontainer/resources/afterInitPostCreateCommands.sh
+++ .devcontainer/resources/afterInitPostCreateCommands.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 if [ ! -f ".env" ]; then cp .env.example .env; fi
 
+# If theres no .npmrc ask for it
+if [ ! -f ".npmrc" ]; then
+  read -n 1 -p "Set NPM auth token (y/n)? " answer
+  printf "\n"
+  case ${answer:0:1} in
+    y|Y )
+      read -p "Insert token: " token
+      echo "//registry.npmjs.org/:_authToken=${token}" >> .npmrc
+      printf '\n.npmrc created!'
+    ;;
+  esac  
+fi
+
 npm install
 
 node ace migration:fresh
